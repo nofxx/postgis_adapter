f1f540f793339e2301a21af2f763aeb7866ac786
Marcos Augusto <x@nofxx.com>
2008-11-26 06:53:44 -0200

    inital commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+class SpatialAdapterNotCompatibleError < StandardError
+end
+
+unless ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+  raise SpatialAdapterNotCompatibleError.
+    new("Only PostgreSQL with PostGIS is supported by the postgis adapter plugin.")
+end
+
+require 'post_gis_adapter'
---
+++ lib/post_gis_functions.rb
@@ -0,0 +1,25 @@
+module PostGisFunctions
+
+  def close_to(point)
+
+  end
+
+
+  def distance(line)
+
+  end
+
+
+
+  def inside?(other)
+
+  end
+
+
+  def contains?(other)
+
+  end
+
+
+
+end
