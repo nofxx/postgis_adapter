65577050b5d5fadba59c4aceafc792b28302fd7b
Marcos Augusto <x@nofxx.com>
2008-11-29 05:59:10 -0200

    way better..

--- lib/postgis_functions.rb
+++ lib/postgis_functions.rb
@@ -8,62 +8,100 @@
 #
 module PostgisFunctions
 
-  # Postgis Functions:
+  # #
+  # Measurement:
   #
-  # ST_Contains  ST_Covers   ST_Within   ST_CoveredBy  ST_DWithin
-  # ST_Overlaps  ST_Relate   ST_Disjoint
-  # ST_Touches   ST_Crosses  ST_Intersects
-  # ST_Equals    ST_Azimuth
-  # ST_Distance  ST_distance_sphere  ST_distance_spheroid ST_max_distance
-  # ST_length    ST_length_spheroid  length3d_spheroid
-  # ST_Area      ST_Perimeter
-  def construct_geometric_sql(type,a,b)
+  # ST_length(geometry)
+  # ST_length_spheroid
+  # length3d_spheroid
+  #
+  # ST_Area(geometry)
+  # ST_Perimeter
+  # ST_Azimuth
+  #
+  # ST_Centroid(geometry)
+  #
+  #
+  # #
+  # Relationship:
+  #
+  # ST_Equals(geometry, geometry)   - Spatially equal
+  #
+  # ST_Distance(geometry, geometry) - Cartesian
+  # ST_distance_sphere
+  # ST_distance_spheroid
+  # ST_max_distance
+  #
+  # ST_DWithin(geometry, geometry, float) - if geom is within dist(float)
+  #
+  # ST_Intersects(geometry, geometry) -  Do not call with a GeometryCollection as an argument
+  # ST_Touches(geometry, geometry) -  ST_Crosses  - Same?
+  #
+  # ST_Within(geometry, geometry) -  - A has to be completely inside B.
+  # ST_Contains ST_Overlaps
+  # ST_Covers(geometry, geometry)  #  ST_CoveredBy - Returns 1 if no point in Geometry B is outside Geometry A
+  #
+  # ST_Relate(geometry, geometry, intersectionPatternMatrix)
+  # ST_Disjoint(geometry, geometry)
+  #
+  # Returns 1 (TRUE)
+  def construct_geometric_sql(type,geoms,options)
 
-   tables = [a,b].map { |t| t.class.to_s.downcase.pluralize }
-   fields = tables.map { |f| f + ".geom" }
-   operation = type.to_s
-   operation.capitalize! unless operation =~ /length|spher|max/
-   operation = "ST_#{operation}" unless operation =~ /th3d/
-
+    tables = geoms.map do |t| {
+      :class => t.class.to_s.downcase.pluralize,
+      :uid =>  unique_identifier,
+      :id => t[:id] }
+    end
+    fields = tables.map { |f| f[:uid] + ".geom" }       # W1.geom
+    fields << options if options
+    froms = tables.map { |f| "#{f[:class]} #{f[:uid]}"}  # streets W1
+    wheres = tables.map { |f| "#{f[:uid]}.id = #{f[:id]}"} # W1.id = 5
+    operation = type.to_s
+    operation.capitalize! unless operation =~ /spher|max/
+    operation = "ST_#{operation}" unless operation =~ /th3d/
+    join_method = " AND "
     sql =   "SELECT #{operation}(#{fields.join(",")}) "+
-            "FROM #{tables.join(",")} "+
-            "WHERE #{tables[0]}.id=#{a[:id]} "
-    sql <<  "AND #{tables[1]}.id=#{b[:id]} " if b
+           "FROM #{froms.join(",")} "
+    sql <<  "WHERE #{wheres.join(join_method)}" if wheres
     p sql
     sql
-   end
-
-  def execute_geometric_calculation(operation, subject, options)#0# column, options) #:nodoc:
-    value = connection.select_value(construct_geometric_sql(operation, subject, options))
-    #type_cast_calculated_value(value, column, operation)
   end
 
-  def execute_relational_calculation(operation, subjects, options) #:nodoc:
-    value = connection.select_value(construct_geometric_sql(operation, subjects[0], subjects[1]))#, options))
+  def execute_geometrical_calculation(operation, subject, options) #:nodoc:
+    value = connection.select_value(construct_geometric_sql(operation, subject, options))
     if value =~ /^\D/
       to_bool(value)
-    else
+    elsif value =~ /\./
       value.to_f
+    else
+      GeoRuby::SimpleFeatures::Geometry.from_hex_ewkb(value) rescue nil
     end
-    #type_cast_calculated_value(value, column, operation)
   end
 
-  def calculate(operation, subject, options = {})
-    if subject.instance_of?(Array)
-      return execute_relational_calculation(operation, subject, options)
-    else
-      return execute_geometric_calculation(operation, subject, options)
-    end
+  def calculate(operation, subject, options = nil)
+    subject = [subject] unless subject.respond_to?(:map)
+    return execute_geometrical_calculation(operation, subject, options)
   end
 
    def to_bool val
      {"f" => false, "t" => true}[val]
    end
+
+   def unique_identifier
+     @u_id ||= "W1"
+     @u_id = @u_id.succ
+   end
+
+   def spatially_equal?(other)
+     calculate(:equals, [self, other])
+   end
 end
 
-
-
-# #
+###
+##
+#
+#
+#
 # POINT
 #
 #
@@ -95,6 +133,10 @@ module PointFunctions
         calculate(:distance, [self, other])
       end
 
+      def spherical_distance other
+        calculate(:distance_sphere, [self, other])
+      end
+
       def inside? other
         calculate(:contains, [other, self])
       end
@@ -102,17 +144,30 @@ module PointFunctions
       def outside? other
         !inside? other
       end
+
+      def in_bounds?(other,margin=500)
+        calculate(:dwithin, [self, other], margin)
+      end
     end
   end
 end
 
-
-# #
+###
+##
+#
+#
+#
 # LINESTRING
 #
 #
 #
+# Linear Referencing
 #
+# ST_line_interpolate_point
+# ST_line_substring
+# ST_line_locate_point
+# ST_locate_along_measure
+# ST_locate_between_measures
 #
 module LineStringFunctions
 
@@ -135,26 +190,39 @@ module LineStringFunctions
         find(:all, :order => "length(geom) #{sort}" )
       end
 
+      def longest
+        find(:first, :order => "length(geom) DESC")
+      end
+
     end
 
     module InstanceMethods
+      include PostgisFunctions
+
       def length
-        self.class.connection.select_value("SELECT length(geom) FROM #{self.class.to_s.pluralize} WHERE id=#{self[:id]}").to_f
+        calculate(:length, self)
       end
 
-
       def intersects? other
-        to_bool(self.class.count_by_sql(geometric_query("intersects", self, other)))
+        calculate(:intersects, [self, other])
+      end
+
+      def crosses? other
+        calculate(:crosses, [self, other])
+      end
+
+      def envelope
+        calculate(:envelope, self)
       end
     end
   end
 end
 
-
-
-
-
-# #
+###
+##
+#
+#
+#
 # Polygon
 #
 #
@@ -194,7 +262,7 @@ module PolygonFunctions
       include PostgisFunctions
 
       def area
-        self.class.connection.select_value("SELECT Area(geom) FROM #{self.class.to_s.pluralize} WHERE id=#{self[:id]}").to_f
+        calculate(:area, self)
       end
 
       def contains? other
@@ -210,12 +278,78 @@ module PolygonFunctions
         calculate(:covers, [self, other])
       end
 
+      def touches? other
+        calculate(:touches, [self, other])
+      end
     end
   end
 end
 
+#POINT(0 0)
+#LINESTRING(0 0,1 1,1 2)
+#POLYGON((0 0,4 0,4 4,0 4,0 0),(1 1, 2 1, 2 2, 1 2,1 1))
+#MULTIPOINT(0 0,1 2)
+#MULTILINESTRING((0 0,1 1,1 2),(2 3,3 2,5 4))
+#MULTIPOLYGON(((0 0,4 0,4 4,0 4,0 0),(1 1,2 1,2 2,1 2,1 1)), ..)
+#GEOMETRYCOLLECTION(POINT(2 3),LINESTRING((2 3,3 4)))
 
+#BBOX OPERATORS
+#These operators utilize indexes. They compare bounding boxes of 2 geometries
+#A &< B (A overlaps or is to the left of B)
+#A &> B (A overlaps or is to the right of B)
+#A << B (A is strictly to the left of B)
+#A >> B (A is strictly to the right of B)
+#A &<| B (A overlaps B or is below B)
+#A |&> B (A overlaps or is above B)
+#A <<| B (A strictly below B)
+#A |>> B (A strictly above B)
+#A = B (A bbox same as B bbox)
+#A @ B (A completely contained by B)
+#A ~ B (A completely contains B)
+#A && B (A and B bboxes interact)
+#A ~= B - true if A and B geometries are binary equal?
 
+#Accessors
+#ST_Dimension
+#ST_Dump
+#ST_EndPoint
+#ST_Envelope
+#ST_ExteriorRing
+#ST_GeometryN
+#ST_GeometryType
+#ST_InteriorRingN
+#ST_IsClosed
+#ST_IsEmpty
+#ST_IsRing
+#ST_IsSimple
+#ST_IsValid
+#ST_mem_size
+#ST_M
+#ST_NumGeometries
+#ST_NumInteriorRings
+#ST_NumPoints
+#ST_npoints
+#ST_PointN
+#ST_SetSRID
+#ST_StartPoint
+#ST_Summary1
+#ST_X
+#ST_XMin,ST_XMax
+#ST_Y
+#YMin,YMax
+#ST_Z
+#ZMin,ZMax
+
+#OUTPUT
+
+#ST_AsBinary
+#ST_AsText
+#ST_AsEWKB
+#ST_AsEWKT
+#ST_AsHEXEWKB
+#ST_AsGML
+#ST_AsKML
+#ST_AsSVG
 
 #if Object.const_defined?("ActiveRecord")
 # ActiveRecord::Base.send(:include, PostgisFunctions)
--- spec/postgis_functions_spec.rb
+++ spec/postgis_functions_spec.rb
@@ -20,9 +20,9 @@ describe "PostgisFunctions" do
     @a1 ||= Area.create!(:data => "Area1", :geom => Polygon.from_coordinates([[[12,45],[45,41],[4,1],[12,45]],[[2,5],[5,1],[14,1],[2,5]]],123))
     @a2 ||= Area.create!(:data => "Area1", :geom => Polygon.from_coordinates([[[22,66],[65,65],[20,10],[22,66]],[[10,15],[15,11],[34,14],[10,15]]],123))
     @a3 ||= Area.create!(:data => "Area3", :geom => Polygon.from_coordinates([[[12.4,-45.3],[45.4,41.6],[4.456,1.0698],[12.4,-45.3]],[[2.4,5.3],[5.4,1.4263],[14.46,1.06],[2.4,5.3]]],123))
-    @r1 ||= Route.create!(:data => "Route1", :geom => LineString.from_coordinates([[1,1],[3,3]],123))
-    @r2 ||= Route.create!(:data => "Route2", :geom => LineString.from_coordinates([[4,4],[6,6]],123))
-    @r3 ||= Route.create!(:data => "Route3", :geom => LineString.from_coordinates([[7,7],[18,18]],123))
+    @r1 ||= Route.create!(:data => "Route1", :geom => LineString.from_coordinates([[1,1],[2,2]],123))
+    @r2 ||= Route.create!(:data => "Route2", :geom => LineString.from_coordinates([[4,4],[7,7]],123))
+    @r3 ||= Route.create!(:data => "Route3", :geom => LineString.from_coordinates([[8,8],[18,18]],123))
     @p1 ||= Position.create!(:data => "Point1", :geom => Point.from_x_y(1,1,123))
     @p2 ||= Position.create!(:data => "Point2", :geom => Point.from_x_y(5,5,123))
     @p3 ||= Position.create!(:data => "Point3", :geom => Point.from_x_y(8,8,123))
@@ -36,36 +36,71 @@ describe "PostgisFunctions" do
       Position.close_to(@p1,123).data.should == @p1.data
     end
 
-    it "should find the distance from a linestring" do
+    it "distance to a linestring" do
       @p1.distance(@r2).should be_close(4.24264068711928, 0.0001)
     end
 
-    it "should find the distance from a polygon" do
-      @p1.distance(@r2).should be_close(4.24264068711928, 0.0001)
+    it "distance to another point" do
+      @p1.distance(@p2).should be_close(5.65685424949238, 0.0001)
     end
 
-    it "should see if point is in area" do
+    it "distance to a polygon" do
+      @p1.distance(@a2).should be_close(21.0237960416286, 0.0001)
+    end
+
+    it "should select the spherical distance" do
+     @p1.spherical_distance(@p2).should be_close(628516.874554178, 0.0001)
+    end
+
+    it "inside area?" do
       @p1.inside?(@a1).should be_false
     end
 
-    it "should see if point is outside an area" do
+    it "ouside area?" do
       @p1.outside?(@a1).should be_true
+    end
+
+    it "in bounds of a geometry?" do
+      @p1.in_bounds?(@r1).should be_true
+    end
+
+    it "in bounds of a geometry? with option" do
+      @p3.in_bounds?(@r1, 1).should be_false
     end
 
   end
 
 
-  describe "Linestring" do
+  describe "LineString" do
 
-
-
-    it "should sort linestring by size" do
-      Route.by_size.first.data.should == "Route1" #[@a1, @a2, @a3]
-    #  Route.by_size.last.data.should == "Route3"
+    it "should sort by size" do
+      Route.by_size.first.data.should == "Route1"
+      Route.by_size.last.data.should == "Route3"
     end
 
-    it "should find the length of a linestring" do
-      @r1.length.should be_close(2.82842712474619, 0.000001)
+    it "largest" do
+      Route.longest.data.should == "Route3"
+    end
+
+    it "its length" do
+      @r1.length.should be_close(1.4142135623731, 0.000001)
+    end
+
+    it "crosses ?" do
+      @r1.crosses?(@r2).should be_false
+    end
+
+    it "should check for crosses" do
+      @r4 ||= Route.create!(:data => "Route3", :geom => LineString.from_coordinates([[10,8],[15,18]],123))
+      @r4.intersects?(@r3).should be_true
+    end
+
+    it "should get a polygon for envelope" do
+      @r1.envelope.should be_instance_of(Polygon)#eql(1)
+    end
+
+    it "should get a polygon for envelope" do
+      @r1.envelope[1].should be_instance_of(Polygon)#eql(1)
     end
 
   end
@@ -73,30 +108,37 @@ describe "PostgisFunctions" do
 
   describe "Polygon" do
 
-    it "should sort area by size" do
+    it "sort by area size" do
       Area.by_size.first.data.should == "Area1" #[@a1, @a2, @a3]
      # Area.by_size.last.data.should == "Area1"
     end
 
-    it "should calculate the area size" do
-      @a1.area.should be_close(724.0, 0.1)
+    it "total area" do
+      @a3.area.should be_close(1093.270089, 0.1)
     end
 
-    it "should calculate the area size" do
+    it "total area 2" do
       @a2.area.should be_close(1159.5, 0.1)
     end
 
-    it "should see if area has pointa" do
+    it "contains points?" do
       @a1.contains?(@p1).should be_false
     end
 
+    it "contains created point?" do
+      @p4 ||= Position.create!(:data => "Point4", :geom => Point.from_x_y(30,30,123))
+      @a1.contains?(@p4).should be_true
+    end
 
+    it "calculate spatially equality" do
+      @a1.spatially_equal?(@a2).should be_false
+    end
 
-    it "should find areas that contains a point" do
+    it "find all areas that contains a point" do
       Area.contains(@p1.geom, 123).should eql([])
     end
 
-    it "should see if point is covered area" do
+    it "area covers point?" do
       @a1.covers?(@p1).should be_false
     end
 
