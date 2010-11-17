e6b97619cf14fd64e1f69480b20d37083c3dbfba
Daniel Berger <dberger@globe.gov>
2010-11-17 09:15:34 -0700

    Updated init.rb to fix the change in the Arel module hierarchy so that the geometry type works.

--- rails/init.rb
+++ rails/init.rb
@@ -1,26 +1,14 @@
-# Patching Arel to support geometry type
-# https://rails.lighthouseapp.com/projects/8994/tickets/4270-arel-032-broke-non-standard-column-types
-# https://rails.lighthouseapp.com/projects/8994/tickets/5194-activerecord-beta-4-does-not-understand-most-postgres-data-types
-
+# Patch Arel to support geometry type.
 module Arel
-  module Sql
-    module Attributes
-      def self.for(column)
+  module Attributes
+    class << self
+      alias original_for for
+
+      def for(column)
         case column.type
-        when :string    then String
-        when :text      then String
-        when :integer   then Integer
-        when :float     then Float
-        when :decimal   then Decimal
-        when :date      then Time
-        when :datetime  then Time
-        when :timestamp then Time
-        when :time      then Time
-        when :binary    then String
-        when :boolean   then Boolean
-        when :geometry  then String
+        when :geometry then String
         else
-          raise NotImplementedError, "Column type `#{column.type}` is not currently handled"
+          original_for(column)
         end
       end
     end
@@ -30,9 +18,11 @@ end
 class SpatialAdapterNotCompatibleError < StandardError
 end
 
-unless ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-  raise SpatialAdapterNotCompatibleError.
-    new("Database config file not set or it does not map to PostgreSQL\n" +
-        "Only PostgreSQL with PostGIS is supported by postgis_adapter.")
+unless ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
+  error_message = "Database config file not set or it does not map to "
+  error_message << "PostgreSQL.\nOnly PostgreSQL with PostGIS is supported "
+  error_message << "by postgis_adapter.")
+  raise SpatialAdapterNotCompatibleError.new(error_message)
 end
+
 require 'postgis_adapter'
